b2d655cd5cdf9e44706452a36d111eba6569f5a5
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-10-29 01:43:18 +1200

    Add the home page

--- multipagewebsite/app/page.tsx
+++ multipagewebsite/app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [afterLoading, setAfterLoading] = useState(false);
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setAfterLoading(true);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <></>
+    <main className={`${afterLoading ? "beforeLoading" : "afterPageLoad"} max-w-[370px] sm:max-w-[500px] my-auto mx-auto`}>
+      <h1>About Us</h1>
+      <p>This is the Home page</p>
+    </main>
   );
 }
