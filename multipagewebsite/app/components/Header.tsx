b40fde4b9eb89735cae2fb665ad78561824e83b5
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-10-29 01:44:34 +1200

    Add the nav and link tags.

--- multipagewebsite/app/components/Header.tsx
+++ multipagewebsite/app/components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
       <h2 className='text-2xl font-bold my-auto '>Welcome to My Multi-Page Website</h2>
       <nav> 
         <ul className='flex gap-3 justify-center items-center'>
-        <li className='hover:text-green-500 hover:border-b-orange-500 hover:border-b-2 '><Link href="/">About</Link></li>
+        <li className='hover:text-green-500 hover:border-b-orange-500 hover:border-b-2'><Link href="/">Home</Link></li>
           <li className='hover:text-green-500 hover:border-b-orange-500 hover:border-b-2 '><Link href="/about">About</Link></li>
           <li className='hover:text-green-500 hover:border-b-orange-500 hover:border-b-2'><Link href="/our-services">Services</Link></li>
           <li className='hover:text-green-500 hover:border-b-orange-500 hover:border-b-2'><Link href="/contact">Contact</Link></li>
